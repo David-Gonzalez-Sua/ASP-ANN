84ec9ff2ee8ac4f96260a5d9335a7cf899a4f1e9
David <dgjsua@gmail.com>
2025-11-02 13:53:09 -0700

    Edited the documentation

--- image_manipulation/image_manipulator.sh
+++ image_manipulation/image_manipulator.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Usage: bash ./image_manipulation/image_manipulator.sh <Manipulation Type> <Image Index>
+#   where <Manipulation Type> is the name of the manipulation (e.g., color_invert)
+#         <Image Index> is the index of the image in the MNIST dataset (optional
 #
 # Inverting Colors: bash ./image_manipulation/image_manipulator.sh color_invert
 #
