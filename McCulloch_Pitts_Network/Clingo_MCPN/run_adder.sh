702f78ef2a44ce57342a8bf07f2b01247bdd7a80
David Gonzalez <dgjsua@gmail.com>
2026-04-04 20:42:42 -0600

    fixed some stuff

--- McCulloch_Pitts_Network/Clingo_MCPN/run_adder.sh
+++ McCulloch_Pitts_Network/Clingo_MCPN/run_adder.sh
@@ -9,8 +9,10 @@
 #
 
 
-# NOTE: Change this to your Python path which includes necessary packages
-PYTHON="/mnt/c/Users/dgjsu/anaconda3/envs/potcassco/python.exe"
+source "venv/bin/activate" \
+  || { echo "Error: could not activate venv. Make sure you have run 'python3 -m venv venv' from the project root."; 
+  exit 1; }
+echo "Successfully activated Python virtual environment."
 
 input_a="$1"
 input_b="$2"
@@ -28,35 +30,44 @@ if [ -z "$3" ]; then
 fi
 
 # Filenames 
-eight_bit_adder="./McCulloch-Pitts_Network/Clingo_MCPN/8bit_adder.lp"
-full_adder="./McCulloch-Pitts_Network/Clingo_MCPN/full_adder_network.lp"
-neuron_def="./McCulloch-Pitts_Network/Clingo_MCPN/logical_neuron.lp"
-decimal_binary_converter_clingo="./McCulloch-Pitts_Network/Clingo_MCPN/decimal_binary_converter.lp"
-decimal_binary_converter_python="./McCulloch-Pitts_Network/Clingo_MCPN/decimal_binary_converter_for_clingo.py"
-default_input="./McCulloch-Pitts_Network/TestingAndDebugging/temp_binary_number.lp"
-graph_visualizer="./McCulloch-Pitts_Network/Clingo_MCPN/graph_visualizer.py"
-graph_code="./McCulloch-Pitts_Network/Clingo_MCPN/network_visualizer.txt"
-graph_pdf="./McCulloch-Pitts_Network/Clingo_MCPN/network_visualizerPOSTER.pdf"
-binary_input="./McCulloch-Pitts_Network/Clingo_MCPN/binary_input.lp"
+eight_bit_adder="./McCulloch_Pitts_Network/Clingo_MCPN/8bit_adder.lp"
+full_adder="./McCulloch_Pitts_Network/Clingo_MCPN/full_adder_network.lp"
+neuron_def="./McCulloch_Pitts_Network/Clingo_MCPN/logical_neuron.lp"
+decimal_binary_converter_clingo="./McCulloch_Pitts_Network/Clingo_MCPN/decimal_binary_converter.lp"
+decimal_binary_converter_python="./McCulloch_Pitts_Network/Clingo_MCPN/decimal_binary_converter_for_clingo.py"
+default_input="./McCulloch_Pitts_Network/TestingAndDebugging/temp_binary_number.lp"
+graph_visualizer="./McCulloch_Pitts_Network/Clingo_MCPN/graph_visualizer.py"
+graph_code="./McCulloch_Pitts_Network/Clingo_MCPN/network_visualizer.dot"
+graph_pdf="./McCulloch_Pitts_Network/Clingo_MCPN/network_visualizer.pdf"
+binary_input="./McCulloch_Pitts_Network/Clingo_MCPN/binary_input.lp"
+clingo_output="./McCulloch_Pitts_Network/Clingo_MCPN/clingo_output.txt"
 
 if [ -z "$2" ]; then
   # Uses default input values
-  clingo "$eight_bit_adder" "$full_adder" "$neuron_def" "$default_input" | tee >($PYTHON "$graph_visualizer") | $PYTHON "$decimal_binary_converter_python" "-f 1"
+  clingo "$eight_bit_adder" "$full_adder" "$neuron_def" "$default_input" \
+  | tee "$clingo_output" \
+  | PYTHON "$decimal_binary_converter_python" "-f 1"
   echo "✅ Created McCulloch-Pitts 8-Bit Adder with default values."
-  echo "✅ Visualized network created as pdf."
+
+  PYTHON "$graph_visualizer" < "$clingo_output" > "$graph_code"
+  dot "$graph_code" "-T" "pdf" "-o" "${graph_pdf}"
+  echo "✅ Visualized network created as PDF."
 
 else
   # Uses Clingo input converter
   # clingo "$eight_bit_adder" "$full_adder" "$neuron_def" "$default_input" "-c input_a=${input_a} -c input_b=${input_b} -c bits=${num_bits}"
 
   # Uses Python input converter
-  $PYTHON "$decimal_binary_converter_python" "-f 0" "-a ${input_a}" "-b ${input_b}" "-s ${num_bits}" > "$binary_input"
+  PYTHON "$decimal_binary_converter_python" "-f 0" "-a ${input_a}" "-b ${input_b}" "-s ${num_bits}" > "$binary_input"
   clingo "$eight_bit_adder" "$full_adder" "$neuron_def" "$binary_input" \
-  | tee >($PYTHON "$graph_visualizer" > "$graph_code") \
-  | $PYTHON "$decimal_binary_converter_python" "-f 1"
-  # | tee >($PYTHON "$graph_visualizer" | dot "-T pdf" "-o ${graph_pdf}") \
+  | tee "$clingo_output" \
+  | PYTHON "$decimal_binary_converter_python" "-f 1"
 
+  echo "✅ Created McCulloch-Pitts 8-Bit Adder with inputs ${input_a} and ${input_b}."
+  
+  PYTHON "$graph_visualizer" < "$clingo_output" > "$graph_code"
   dot "$graph_code" "-T" "pdf" "-o" "${graph_pdf}"
+  echo "✅ Visualized network created as PDF."
 
 fi
 
